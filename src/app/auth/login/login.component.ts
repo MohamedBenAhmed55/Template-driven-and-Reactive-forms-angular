96a60e493b18b0758e1a6335312fca7973e0ac4a
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2026-02-08 20:45:43 +0100

    Updating Form Values Programmatically

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,10 @@
-import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  DestroyRef,
+  inject,
+  viewChild,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { debounceTime } from 'rxjs';
 
@@ -10,29 +16,47 @@ import { debounceTime } from 'rxjs';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  private form = viewChild.required<NgForm>('form')
-  private destroyRef = inject(DestroyRef)
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
 
   constructor() {
     afterNextRender(() => {
-      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
-        next: (value) => window.localStorage.setItem('saved-login-form', JSON.stringify({email: value.email}))
-      });
+      const savedForm = window.localStorage.getItem('saved-login-form');
+      if (savedForm) {
+        const loadedFormData = JSON.parse(savedForm);
+        const savedEmail = loadedFormData.email;
+        // this.form().setValue({ email: savedEmail, password: '' });
+        setTimeout(() =>{
+          this.form().controls['email'].setValue(savedEmail)
+        },1)
+      }
 
-      this.destroyRef.onDestroy(() => {subscription?.unsubscribe() })
-    })
+      const subscription = this.form()
+        .valueChanges?.pipe(debounceTime(500))
+        .subscribe({
+          next: (value) =>
+            window.localStorage.setItem(
+              'saved-login-form',
+              JSON.stringify({ email: value.email }),
+            ),
+        });
+
+      this.destroyRef.onDestroy(() => {
+        subscription?.unsubscribe();
+      });
+    });
   }
 
   onSubmit(formData: NgForm) {
-    if(formData.form.invalid){
+    if (formData.form.invalid) {
       return;
     }
 
-    const enteredEmail = formData.form.value.email
-    const enteredPassword = formData.form.value.password
+    const enteredEmail = formData.form.value.email;
+    const enteredPassword = formData.form.value.password;
 
-    console.log(formData.form)
-    console.log(enteredEmail, enteredPassword)
+    console.log(formData.form);
+    console.log(enteredEmail, enteredPassword);
     formData.form.reset();
   }
 }
