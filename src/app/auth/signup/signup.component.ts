e66f9579c170a58e8d5af9bf6ae6c09615d0a3ef
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2026-02-12 00:15:40 +0100

    Creating Multi-Input Validators / Form Group Validators

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,21 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
+
+function equalValues(controlName1: string, controlName2: string){
+
+  return (control: AbstractControl) => {
+    const val1 = control.get(controlName1)?.value
+    const val2 = control.get(controlName2)?.value
+
+    if(val1 === val2){
+      return null;
+    }
+
+    return {valuesNotEqual: true}
+  }
+  
+}
 @Component({
   selector: 'app-signup',
   standalone: true,
@@ -14,6 +29,8 @@ export class SignupComponent {
     passwords: new FormGroup({
       password: new FormControl('', {validators: [Validators.required, Validators.minLength(6)]}),
       confirmPassword: new FormControl('', {validators:[Validators.required, Validators.minLength(6)]}),
+    }, {
+      validators: [equalValues('password', 'confirmPassword')]
     }),
     firstName: new FormControl('', {validators: [Validators.required]}),
     lastName: new FormControl('', {validators: [Validators.required]}),
